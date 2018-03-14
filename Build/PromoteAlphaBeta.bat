38427cc1716ab79925f96a46fb2e0e509d2bf093
Jack Wallace <jack@astutepixel.com>
2018-03-14 00:30:11 +0000

    Deactivate the MirrorServer services whilst we rsync files.

--- Build/PromoteAlphaBeta.bat
+++ Build/PromoteAlphaBeta.bat
@@ -1,6 +1,11 @@
 @echo off
 
 cd C:\Work\Trunk\DarkSpace\Build\
+
+rem Stop MirrorServers for Beta builds during rsync below
+..\..\GameCQ\Bin\ProcessClientCLI vm3.palestar.com.ini STOP 9102:MirrorServer - DarkSpace (Beta)
+..\..\GameCQ\Bin\ProcessClientCLI vm3.palestar.com.ini STOP 9201:MirrorServer - GCQS (Beta)
+
 ..\..\GameCQ\Bin\ProcessClientCLI vm1.palestar.com.ini START PromoteAlphaBeta 1
 if errorlevel 1 goto :Error
 ..\..\GameCQ\Bin\ProcessClientCLI vm1.palestar.com.ini START PromoteBetaExternal 1
@@ -8,10 +13,14 @@ if errorlevel 1 goto :Error
 
 :Done
 echo Promoted Alpha to Beta...
+..\..\GameCQ\Bin\ProcessClientCLI vm3.palestar.com.ini START 9102:MirrorServer - DarkSpace (Beta) 1
+..\..\GameCQ\Bin\ProcessClientCLI vm3.palestar.com.ini START 9201:MirrorServer - GCQS (Beta) 1
 exit /B 0
 
 :Error
 echo Error occured while promoting Alpha to Beta...
+..\..\GameCQ\Bin\ProcessClientCLI vm3.palestar.com.ini START 9102:MirrorServer - DarkSpace (Beta) 1
+..\..\GameCQ\Bin\ProcessClientCLI vm3.palestar.com.ini START 9201:MirrorServer - GCQS (Beta) 1
 exit /B 1
 
 rem pause
