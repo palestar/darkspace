817932fed1c4210bbdcb584bddc83ae059e8e9fc
WalrusofApathy <35180009+WalrusofApathy@users.noreply.github.com>
2018-04-01 20:27:44 -0400

    Initial Files to implement the Quantum Higgs Net, a gadget to increase an enemies ships mass

---
+++ Gadgets/QuantumHiggsNet.h
@@ -0,0 +1,65 @@
+#ifndef INCREASE_MASS_H 
+#define INCREASE_MASS_H
+
+#include "DarkSpace/GadgetModifierEW.h"
+
+//----------------------------------------------------------------------------
+
+class QuantumHiggsNet : public GadgetModifierEW 
+{
+public:
+	DECLARE_WIDGET_CLASS();
+	Type type() const
+	{
+		return SPECIAL_OFFENSIVE;
+	}
+	int	addValue() const
+	{
+		return 1200;
+	}
+	int buildTechnology() const
+	{
+		return 10;
+	}
+	int	buildCost() const
+	{
+		return 300;
+	}
+	dword buildFlags() const
+	{
+		return NounPlanet::FLAG_METALS;
+	}
+	int	buildTime() const
+	{
+		return 140;
+	}
+	int	energyCost() const
+	{
+		return 10;
+	}
+	float strength() const
+	{
+		return 100.0f;
+	}
+	float range() const
+	{
+		return 300.0f;
+	}
+	int cooldown() const
+	{
+		return TICKS_PER_SECOND * 30;
+	}
+	ModifierType modifierType() const
+	{
+		return MT_MASS;
+	}
+};
+
+//----------------------------------------------------------------------------
+
+
+
+#endif
+
+//----------------------------------------------------------------------------
+// EOF
