a9dce4582a16ac15244d23d724b7d04a14a7483f
Arnaud Lefebvre <a.lefebvre@outlook.fr>
2018-04-08 23:13:03 +0200

    Set cores to 2 and fix directory name

--- Build/BuildLinuxServer.sh
+++ Build/BuildLinuxServer.sh
@@ -15,7 +15,7 @@ build() {
   for build in "${builds[@]}"; do
     echo "Building ${directory}/${build}..."
     pushd "${build}"
-    make BUILD_CONFIGURATION="ReleaseLinux" CC=$CC CXX=$CXX INTERMEDIATE_DIR="ReleaseLinux/obj" DESTINATION_DIR="../Bin"
+    make -j2 BUILD_CONFIGURATION="ReleaseLinux" CC=$CC CXX=$CXX INTERMEDIATE_DIR="ReleaseLinux/obj" DESTINATION_DIR="../Bin"
     popd
   done
 
@@ -25,7 +25,7 @@ build() {
 main() {
   pushd "${WORK}"
   build Medusa "${medusa_builds[@]}"
-  build DarkSpace "${darkspace_builds[@]}"
+  build darkspace "${darkspace_builds[@]}"
   build GameCQ "${gamecq_builds[@]}"
 }
 
